ab91c3688437b15fd75bc67466e91e137836e273
tsaylor37 <10839367+tsaylor37@users.noreply.github.com>
2018-10-03 13:16:21 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "Enter Your API Key here"
+consumer_secret = "Enter Your API Key here"
+access_token = "Enter Your API Key here"
+access_token_secret = "Enter Your API Key here"
